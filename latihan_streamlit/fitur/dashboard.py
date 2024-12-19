70bd7b83b479b4456898558e1e948bae6a30b98b
Zhaharaaaaa <zhahara192@gmail.com>
2024-12-19 19:47:20 +0700

    Add files via upload

---
+++ latihan_streamlit/fitur/dashboard.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Halaman Dashboard")
+st.image("./latihan_streamlit/1.jpg", caption = "Ocean")
+
+# fungsi menghitung dan menyimpan total
+def total():
+    total_nabung = sum(t["jumlah"]
+                       for t in st.session_state
+                       ["total_semua"] if t ["Tipe"]=="Menabung")
+    return total_nabung
+
+total_semua = st.session_state["total_semua"]
+total_nabung = total()
+
+st.metric("Total Menabung", f"Rp {total_nabung}")
+    
+
